7e7f4efa4f92e92f75f1afdffd8816b35c5d36c9
Rahil-Parikh <rahil.parikh@somaiya.edu>
2021-04-19 13:13:06 +0530

    Title Page test

---
+++ src/main/java/selenium/react/test/ReactTest.java
@@ -0,0 +1,37 @@
+package selenium.react.test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class ReactTest {
+	public static void main(String[] args) {
+        // declaration and instantiation of objects/variables
+//    	System.setProperty("webdriver.gecko.driver","-/usr/bin/geckodriver");
+		WebDriver driver = new FirefoxDriver();
+		//comment the above 2 lines and uncomment below 2 lines to use Chrome
+		//System.setProperty("webdriver.chrome.driver","G:\\chromedriver.exe");
+		//WebDriver driver = new ChromeDriver();
+    	
+        String baseUrl = "http://0.0.0.0:4200/";
+        String expectedTitle = "React App";
+        String actualTitle = "";
+
+        // launch Fire fox and direct it to the Base URL
+        driver.get(baseUrl);
+
+        // get the actual value of the title
+        actualTitle = driver.getTitle();
+        System.out.println("Page Title : "+actualTitle);
+        /*
+         * compare the actual title of the page with the expected one and print
+         * the result as "Passed" or "Failed"
+         */
+        if (actualTitle.contentEquals(expectedTitle)){
+            System.out.println("Test Passed!");
+        } else {
+            System.out.println("Test Failed");
+        }
+       
+        //close Fire fox
+        driver.close();
+    }
+}
